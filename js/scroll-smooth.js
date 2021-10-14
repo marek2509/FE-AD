ec8898fd73ab9cb5dee93883356f8de34d22d418
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2021-10-14 14:10:39 +0200

    Change from scrollTo to scroll in scroll-smooth.js

--- js/scroll-smooth.js
+++ js/scroll-smooth.js
@@ -1,19 +1,17 @@
 const arrayNavItems = document.querySelectorAll(".link-nav");
 
-
 const checClick = (e) => {
-    e.preventDefault();
-	const actualClickLink = e.target.parentElement.getAttribute("href");
-	const getYSection = document
-		.querySelector(actualClickLink)
-		.offsetTop;
+  e.preventDefault();
+  const actualClickLink = e.target.parentElement.getAttribute("href");
+  const getYSection = document.querySelector(actualClickLink).offsetTop;
 
-	console.log(getYSection);
+  console.log(getYSection);
 
-	window.scrollTo({
-		top: getYSection-80,
-		left: 0,
-		behavior: "smooth",
-	});
+  window.scroll({
+    top: getYSection - 80,
+    left: 0,
+    behavior: "smooth",
+  });
 };
+
 arrayNavItems.forEach((x) => x.addEventListener("click", checClick));
