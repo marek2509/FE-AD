eda5f36b2c4b7a853c72fdfb94cf65726dd78bdc
Marek <marek.wojciechowicz25@gmail.com>
2021-11-18 21:10:50 +0100

    CHamge in fall.js from InterectionObserv to window.Scroll

--- js/fall.js
+++ js/fall.js
@@ -1,38 +1,64 @@
-const fadenLeftItems = document.querySelectorAll(`[data-fall='faden-left']`);
-const fadenRightItems = document.querySelectorAll(`[data-fall='faden-right']`);
+let fadenLeftItems = document.querySelectorAll(`[data-fall='faden-left']`);
+let fadenRightItems = document.querySelectorAll(`[data-fall='faden-right']`);
 
 fadenLeftItems.forEach((x) => x.classList.add("appearNowSetLeft"));
 fadenRightItems.forEach((x) => x.classList.add("appearNowSetRight"));
 
-const options2 = {
-  rootMargin: "-100px",
+// const options2 = {
+//   rootMargin: "-200px",
+// };
+
+// const launchStartCounterLeft = (counterItem) => {
+//   if (counterItem.classList.contains("appearNowLeft")) {
+//     return;
+//   }
+//   const startCounter = (entry) => {
+//     if (entry[0].isIntersecting) {
+//       counterItem.classList.add("appearNowLeft");
+//     }
+//   };
+//   const observer2 = new IntersectionObserver(startCounter);
+//   observer2.observe(counterItem);
+// };
+// fadenLeftItems.forEach((item) => launchStartCounterLeft(item));
+
+// const launchStartCounterRight = (counterItem) => {
+
+//   if (counterItem.classList.contains("appearNowRight")) {
+//     // return;
+//   }
+
+//   const startCounter = (entry) => {
+//    console.log(entry.top);
+//     if (entry[0].isIntersecting) {
+//       counterItem.classList.add("appearNowRight");
+//     }
+//   };
+//   const observer2 = new IntersectionObserver(startCounter, options2);
+//   observer2.observe(counterItem);
+// };
+
+// fadenRightItems.forEach((item) => launchStartCounterRight(item));
+
+//////////////////////////////////////
+
+const handleScrollAnimation = () => {
+	fadenRightItems.forEach((itemAnim) => {
+		if (itemAnim.classList.contains("appearNowRight")) return;
+		// if (window.scrollY >= (itemAnim.offsetTop + itemAnim.offsetHeight)) {
+		if (0 > itemAnim.getBoundingClientRect().top - window.innerHeight * 0.8) {
+			itemAnim.classList.add("appearNowRight");
+		}
+	});
+  
+	fadenLeftItems.forEach((itemAnim) => {
+		if (itemAnim.classList.contains("appearNowRight")) return;
+		// if (window.scrollY >= (itemAnim.offsetTop + itemAnim.offsetHeight)) {
+		if (0 > itemAnim.getBoundingClientRect().top - window.innerHeight * 0.8) {
+			itemAnim.classList.add("appearNowLeft");
+		}
+	});
+
 };
 
-const launchStartCounterLeft = (counterItem) => {
-  if (counterItem.classList.contains("appearNowLeft")) {
-    return;
-  }
-  const startCounter = (entry) => {
-    if (entry[0].isIntersecting) {
-      counterItem.classList.add("appearNowLeft");
-    }
-  };
-  const observer2 = new IntersectionObserver(startCounter);
-  observer2.observe(counterItem);
-};
-fadenLeftItems.forEach((item) => launchStartCounterLeft(item));
-
-const launchStartCounterRight = (counterItem) => {
-  if (counterItem.classList.contains("appearNowRight")) {
-    return;
-  }
-  const startCounter = (entry) => {
-    if (entry[0].isIntersecting) {
-      counterItem.classList.add("appearNowRight");
-    }
-  };
-  const observer2 = new IntersectionObserver(startCounter);
-  observer2.observe(counterItem);
-};
-
-fadenRightItems.forEach((item) => launchStartCounterRight(item));
+window.addEventListener("scroll", handleScrollAnimation);
