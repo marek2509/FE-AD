5a56780e37b38c841df84fe6c9464ba61ca0097e
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2021-11-18 08:28:39 +0100

    Added a counter in achivements

--- js/counter.js
+++ js/counter.js
@@ -1,33 +1,42 @@
-const counterItems = document.querySelectorAll(".achivements__box-number");
+const counterItems = document.querySelectorAll(".counter");
 const counterBox = document.querySelector(".achivements__boxes");
 
+const lastNumberIndexOfItems = counterItems.length - 1;
+
+console.log("length " + lastNumberIndexOfItems);
+
 const options = {
-	rootMargin: "-180px",
+  rootMargin: "-180px",
 };
 
-const startCounter = (entry) => {
-	if (entry[0].isIntersecting) {
-        let i=1;
-		counterItems.forEach((counter) => {
-			console.log(counter);
 
-			const updateCounter = () => {
-				const finalNumber = counter.getAttribute("data-number");
-				let speed = finalNumber/300;
-                const value = i++*speed;
-				if (value < finalNumber) {
-					counter.textContent = `${Math.floor(i++*speed)}`;
 
-                    setTimeout(updateCounter, 20);
-				} else {
-					counter.textContent = finalNumber;
-				}
-			};
-			updateCounter();
-		});
-	}
+const launchStartCounter = (counterItem) => {
+	let i = 0;
+  const startCounter = (entry) => {
+    if (entry[0].isIntersecting) {
+
+        const updateCounter = () => {
+          i++;
+          const finalNumber = counterItem.getAttribute("data-number");
+          let speed = finalNumber / 250;
+          const value = i * speed;
+          if (value < finalNumber) {
+            counterItem.textContent = `${Math.floor(value)}`;
+
+            setTimeout(updateCounter, 2);
+          } else {
+            counterItem.textContent = finalNumber;
+          }
+        };
+
+        updateCounter();
+   
+    }
+  };
+
+  const observer = new IntersectionObserver(startCounter, options);
+  observer.observe(counterItem);
 };
 
-const observer = new IntersectionObserver(startCounter, options);
-observer.observe(counterBox);
-
+counterItems.forEach(item => launchStartCounter(item))
