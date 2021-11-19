001c9ed70836807c65f62db8cdb258619db5da1a
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2021-11-19 09:46:43 +0100

    Finished training section

--- js/main.js
+++ js/main.js
@@ -4,94 +4,94 @@ const navBar = document.querySelector(".container-nav__nav-bar");
 const showCircleShadowCard = document.querySelector("#showCircleShadowCard");
 const cardTextShadow = document.querySelector(".card-shadow .text");
 const arrayNavItems = document.querySelectorAll(
-	".container-nav__nav-bar-list-item-link"
+  ".container-nav__nav-bar-list-item-link"
 );
 const arrowBounceDown = document.querySelector("i.arrow-down");
 const scrollSpySections = document.querySelectorAll(".section");
 
 const showMenu = () => {
-	navBar.classList.toggle("visible-nav");
+  navBar.classList.toggle("visible-nav");
 };
 
 const toggleBurgerX = () => {
-	burger.classList.toggle("burger-x");
-	showMenu();
-	handleNavItemsAnimation();
+  burger.classList.toggle("burger-x");
+  showMenu();
+  handleNavItemsAnimation();
 };
 
 const handleScrollSpy = () => {
-	const sections = [];
-	scrollSpySections.forEach((section) => {
-		// console.log(window.scrollY);
-		// wartość scrolla
-		// console.log(section.offsetTop);
-		// odległośc danej sekcji od górnej krawędzi przeglądarki
-		// console.log(section.offsetHeight);
-		// wysokość każdej z sekcji
+  const sections = [];
+  scrollSpySections.forEach((section) => {
+    // console.log(window.scrollY);
+    // wartość scrolla
+    // console.log(section.offsetTop);
+    // odległośc danej sekcji od górnej krawędzi przeglądarki
+    // console.log(section.offsetHeight);
+    // wysokość każdej z sekcji
 
-		if (
-			window.scrollY >=
-			scrollSpySections[scrollSpySections.length - 1].offsetTop -
-				window.innerHeight +
-				100
-		) {
-			if (section.id == "contact") {
-				sections.push(section);
+    if (
+      window.scrollY >=
+      scrollSpySections[scrollSpySections.length - 1].offsetTop -
+        window.innerHeight +
+        100
+    ) {
+      if (section.id == "contact") {
+        sections.push(section);
 
-				const activeSection = document
-					.querySelector(`[href*="${sections[0].id}"]`)
-					.querySelector("p");
-				arrayNavItems.forEach((item) =>
-					item.querySelector("p").classList.remove("active")
-				);
-				activeSection.classList.add("active");
-			}
-		} else if (
-			window.scrollY <=
-			section.offsetTop + section.offsetHeight - 81
-		) {
-			sections.push(section);
+        const activeSection = document
+          .querySelector(`[href*="${sections[0].id}"]`)
+          .querySelector("p");
+        arrayNavItems.forEach((item) =>
+          item.querySelector("p").classList.remove("active")
+        );
+        activeSection.classList.add("active");
+      }
+    } else if (
+      window.scrollY <=
+      section.offsetTop + section.offsetHeight - 81
+    ) {
+      sections.push(section);
 
-			const activeSection = document
-				.querySelector(`[href*="${sections[0].id}"]`)
-				.querySelector("p");
+      const activeSection = document
+        .querySelector(`[href*="${sections[0].id}"]`)
+        .querySelector("p");
 
-			arrayNavItems.forEach((item) =>
-				item.querySelector("p").classList.remove("active")
-			);
-			activeSection.classList.add("active");
-		}
-	});
+      arrayNavItems.forEach((item) =>
+        item.querySelector("p").classList.remove("active")
+      );
+      activeSection.classList.add("active");
+    }
+  });
 };
 
 //////////////////////
 const deleteAnimation = () => {
-	arrayNavItems.forEach((item) => item.classList.remove("nav-items-animation"));
+  arrayNavItems.forEach((item) => item.classList.remove("nav-items-animation"));
 };
 
 const handleNavItemsAnimation = () => {
-	let delayTime = 0;
-	navBar.style.animationDelay = "." + delayTime++ + "s";
-	arrayNavItems.forEach((item) => {
-		item.classList.toggle("nav-items-animation");
-		item.style.animationDelay = "." + delayTime++ + "s";
-	});
+  let delayTime = 0;
+  navBar.style.animationDelay = "." + delayTime++ + "s";
+  arrayNavItems.forEach((item) => {
+    item.classList.toggle("nav-items-animation");
+    item.style.animationDelay = "." + delayTime++ + "s";
+  });
 };
 
 ///////////////////////////
 
 const checClick = (e) => {
-	e.preventDefault();
-	const actualClickLink = e.target.parentElement.getAttribute("href");
-	const getYSection = document.querySelector(actualClickLink).offsetTop;
-	window.scroll({
-		top: getYSection - 80,
-		left: 0,
-		behavior: "smooth",
-	});
-	deleteAnimation();
-	navBar.classList.remove("visible-nav");
-	burger.classList.remove("burger-x");
+  e.preventDefault();
+  const actualClickLink = e.target.parentElement.getAttribute("href");
+  const getYSection = document.querySelector(actualClickLink).offsetTop;
+  window.scroll({
+    top: getYSection - 80,
+    left: 0,
+    behavior: "smooth",
+  });
+  deleteAnimation();
+  navBar.classList.remove("visible-nav");
+  burger.classList.remove("burger-x");
 };
 
 arrayNavItems.forEach((x) => x.addEventListener("click", checClick));
@@ -99,3 +99,4 @@ arrowBounceDown.addEventListener("click", checClick);
 window.addEventListener("scroll", handleScrollSpy);
 
 burgerBtn.addEventListener("click", toggleBurgerX);
+
